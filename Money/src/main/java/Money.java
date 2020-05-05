afbda1eacf45a1b64c22223a51f3a816a9b99db1
escray <zescray@gmail.com>
2020-05-05 20:34:46 +0800

    add Sum class

--- Money/src/main/java/Money.java
+++ Money/src/main/java/Money.java
@@ -1,4 +1,4 @@
-class Money {
+class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -31,5 +31,9 @@ class Money {
 
     public Money times(int multiplier) {
         return new Money(amount * multiplier, currency);
+    }
+
+    public Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
