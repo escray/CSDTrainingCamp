36588f84158c933f42404e9850cfbdcb4fb34668
escray <zescray@gmail.com>
2020-05-15 23:06:48 +0800

    MarsRover

--- MarsRover/src/main/java/Rover.java
+++ MarsRover/src/main/java/Rover.java
@@ -1,12 +1,19 @@
+import java.util.Optional;
+
 public class Rover {
 
     Direction direction;
+    Coordinate coordinate = new Coordinate(0, 0);
 
-    public Rover() {
+    private Grid grid;
+
+    public Rover(Grid grid) {
+        this.grid = grid;
         direction = Direction.NORTH;
     }
 
     public String execute(String commands) {
+        String obstacleString = "";
         for (char c : commands.toCharArray()) {
             if (c == 'R') {
                 direction = direction.right();
@@ -14,8 +21,16 @@ public class Rover {
             if (c == 'L') {
                 direction = direction.left();
             }
+            if (c == 'M') {
+                obstacleString = move();
+            }
         }
-        return "0:0:" + direction.value();
+        return obstacleString + coordinate.x() + ":" + coordinate.y() + ":" + direction.value();
     }
 
+    private String move() {
+        Optional<Coordinate> nextCoordinate = grid.nextCoordinateFor(this.coordinate, direction);
+        nextCoordinate.ifPresent( nc -> this.coordinate = nc );
+        return nextCoordinate.isPresent() ? "" : "O:";
+    }
 }
--- MarsRover/src/test/java/RoverTest.java
+++ MarsRover/src/test/java/RoverTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+
+import static java.util.Arrays.asList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,10 +15,12 @@ import static org.junit.Assert.assertThat;
 public class RoverTest {
 
     private Rover rover;
+    private Grid grid;
 
     @Before
     public void initialise() {
-        rover = new Rover();
+        grid = new Grid();
+        rover = new Rover(grid);
     }
 
     @Test
@@ -37,6 +42,74 @@ public class RoverTest {
             "LLLL, 0:0:N"
     })
     public void rotate_left(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "M, 0:1:N",
+            "MMM, 0:3:N"
+    })
+    public void move_up(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "MMMMMMMMMM, 0:0:N",
+            "MMMMMMMMMMMMMMM, 0:5:N"
+    })
+    public void wrap_from_top_to_bottom_when_moving_north(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "RM, 1:0:E",
+            "RMMMMM, 5:0:E"
+    })
+    public void move_right(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "RMMMMMMMMMM, 0:0:E",
+            "RMMMMMMMMMMMMMMM, 5:0:E"
+    })
+    public void wrap_from_right_to_left_when_moving_east(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "LM, 9:0:W",
+            "LMMMMM, 5:0:W"
+    })
+    public void move_left(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "LLM, 0:9:S",
+            "LLMMMMM, 0:5:S"
+
+    })
+    public void move_south(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "MMMM, O:0:3:N",
+            "RMMMMMM, O:1:0:E"
+    })
+    public void stop_at_obstacle(String commands, String position) {
+        Coordinate obstacle_0x4 = new Coordinate(0, 4);
+        Coordinate obstacle_2x0 = new Coordinate(2, 0);
+        Grid grid = new Grid(asList(obstacle_0x4, obstacle_2x0));
+        Rover rover = new Rover(grid);
         assertThat(rover.execute(commands), is(position));
     }
 }
