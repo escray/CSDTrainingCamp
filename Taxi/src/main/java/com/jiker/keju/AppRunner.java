ad0fd3cb3500348242a13df27599f520b7ac0942
escray <zescray@gmail.com>
2020-05-18 15:40:17 +0800

    Taxi initial

---
+++ Taxi/src/main/java/com/jiker/keju/AppRunner.java
@@ -0,0 +1,13 @@
+package com.jiker.keju;
+
+public class AppRunner {
+
+    public static void main(String[] args) {
+        String testDataFile = args[0];
+
+
+
+        String receipt = "";
+        System.out.println(receipt);
+    }
+}
