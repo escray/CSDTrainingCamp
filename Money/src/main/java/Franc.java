823852d13cb441e7be693439510682be6ac21a77
escray <zescray@gmail.com>
2020-06-18 17:02:57 +0800

    restructure

---
+++ Money/src/main/java/Franc.java
@@ -0,0 +1,20 @@
+public class Franc {
+
+    int amount;
+
+    public Franc(int amount) {
+
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(this.amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Franc franc = (Franc)obj;
+        return this.amount == franc.amount;
+
+    }
+}
