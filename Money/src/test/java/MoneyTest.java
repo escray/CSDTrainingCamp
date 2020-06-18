a27535bb7111c8d3b56eba9786c7763727ab42dc
escray <zescray@gmail.com>
2020-06-18 16:48:35 +0800

    Money/todo.md

--- Money/src/test/java/MoneyTest.java
+++ Money/src/test/java/MoneyTest.java
@@ -7,16 +7,13 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        assertEquals(10, five.times(2).amount);
-        assertThat(five.times(2).amount, is(10));
-        assertEquals(15, five.times(3).amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
-
-        
     }
 }
